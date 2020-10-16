a69be8f1d5ad7eaa9100502ecd3c67b4aead665f
Erik <erik.daniel.nobleta@oocl.com>
2020-10-16 14:33:44 +0800

    [ParkingLot][Erik-1]test: add test case when parking boy parks 2 cars returns correct cars given parking tickets

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -26,4 +26,19 @@ class ParkingBoyTest {
 
         assertSame(car, fetchedCar);
     }
+
+    @Test
+    void should_return_right_cars_when_parking_boy_fetch_cars_given_tickets() {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+        ParkingTicket parkingTicket1 = parkingBoy.park(car1);
+        ParkingTicket parkingTicket2 = parkingBoy.park(car2);
+
+        Car fetchedCar1 = parkingBoy.fetch(parkingTicket1);
+        Car fetchedCar2 = parkingBoy.fetch(parkingTicket2);
+
+        assertSame(car1, fetchedCar1);
+        assertSame(car2, fetchedCar2);
+    }
 }

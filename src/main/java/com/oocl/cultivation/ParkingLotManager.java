c5c6785b0bd4331421de6b399a25120a9d4b5198
Erik <erik.daniel.nobleta@oocl.com>
2020-10-18 10:11:10 +0800

    [ParkingLot][Erik-1]feature: added commands for parking lot manager

--- src/main/java/com/oocl/cultivation/ParkingLotManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -4,4 +4,12 @@ public class ParkingLotManager {
     public static void assignParkingLot(ParkingJob parkingBoy, ParkingLot lot) {
         parkingBoy.addLot(lot);
     }
+
+    public static ParkingTicket orderPark(ParkingJob parkingBoy, Car car) throws NotEnoughPosition {
+        return parkingBoy.park(car);
+    }
+
+    public static Car orderFetch(ParkingJob parkingBoy, ParkingTicket ticket) throws UnrecognizedParkingTicket, NoTicketException {
+        return parkingBoy.fetch(ticket);
+    }
 }

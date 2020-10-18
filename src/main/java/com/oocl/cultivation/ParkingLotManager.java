c9c621664d6ec023f12c45e65d095f0645303f91
Erik <erik.daniel.nobleta@oocl.com>
2020-10-18 10:23:16 +0800

    [ParkingLot][Erik-1]feature: parking lot manager inherits parking job

--- src/main/java/com/oocl/cultivation/ParkingLotManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotManager.java
@@ -1,8 +1,14 @@
 package com.oocl.cultivation;
 
-public class ParkingLotManager {
+public class ParkingLotManager extends ParkingJob{
+    public ParkingLotManager() { }
+
     public static void assignParkingLot(ParkingJob parkingBoy, ParkingLot lot) {
         parkingBoy.addLot(lot);
+    }
+
+    public static void assignParkingLot(ParkingLotManager parkingLotManager, ParkingLot lot) {
+        parkingLotManager.addLot(lot);
     }
 
     public static ParkingTicket orderPark(ParkingJob parkingBoy, Car car) throws NotEnoughPosition {
